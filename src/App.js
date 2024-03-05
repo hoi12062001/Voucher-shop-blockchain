ba9e39b3f9c50d8089d8dc6585e8bb0b39fbdb01
LeHoii <levanhoi.backenddeveloper@gmail.com>
2024-03-05 23:51:15 +0700

    Start project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListAll from "./ListAll";
-
+import TrangChu from "./TrangChu" ;
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route exact path="/" element={<ListAll />} />
+          <Route exact path="/TrangChu" element={<TrangChu />} />
         </Routes>
       </Router>
     </div>
